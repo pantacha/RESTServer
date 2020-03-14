a84102e863a943c98cf1b25bc8db6b4e32994d73
pantacha <pantacha@hotmail.com>
2020-03-14 19:58:48 +0100

    connection string

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'env') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb + srv: //cafe-user:PssERJrnmSY6M8pU@cluster0-y139h.mongodb.net/cafe';
+    urlDB = 'mongodb+srv://George:PssERJrnmSY6M8pU@cluster0-y139h.mongodb.net/cafe';
 }
 
 process.env.URLDB = urlDB;
